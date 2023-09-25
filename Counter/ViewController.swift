4f47f88925f2111ed54160c358d8c8fad035a557
AminaMag <magomedova_amina2005@mail.ru>
2023-09-25 12:49:57 +0300

    добавлен код для работы счетчика

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var counterImage: UILabel!
+    
+    @IBOutlet weak var buttonImage: UIButton!
+    
+    var number: Int = 0
+    
+    var isTap: Bool = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        counterImage.font = .systemFont(ofSize: 25, weight: .medium)
+        buttonImage.backgroundColor = .blue
+        buttonImage.tintColor = .white
     }
-
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        if isTap {
+            number += 1
+            counterImage.text = "Значение счетчика: \(number)"
+        }
+    }
 }
 
